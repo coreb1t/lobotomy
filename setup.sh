e9dd1f5616adad4e20e31b00527a13d66668a3c9
coreb1t <coreb1t@posteo.net>
2016-11-01 23:22:55 +0100

    setup.sh - replaced build of apktool with direct download

--- setup.sh
+++ setup.sh
@@ -15,12 +15,20 @@ python setup.py install
 cd ..
 
 # Download and build apktool
-git clone git://github.com/iBotPeaches/Apktool.git
-cd Apktool
-./gradlew build fatJar
-mv brut.apktool/apktool-cli/build/libs/apktool-cli.jar ../apktool.jar
-cd ..
-rm -rf Apktool/
+
+# git clone git://github.com/iBotPeaches/Apktool.git
+# cd Apktool
+# ./gradlew build fatJar
+# mv brut.apktool/apktool-cli/build/libs/apktool-cli.jar ../apktool.jar
+# cd ..
+# rm -rf Apktool/
+
+# Download latest release without build apktool
+# TODO fix $apktool_link with curl
+# apktool_version=$(curl -s -I https://github.com/iBotPeaches/Apktool/releases/latest -I | awk '/Location: (.*)/ {print $2}' | tail -n 1)
+# apktool_link="https://github.com$(curl -s $apktool_version | grep 'releases/download' | cut -d '"' -f 2)"
+# wget -O apktool.jar $apktool_link
+wget -O apktool.jar https://github.com/iBotPeaches/Apktool/releases/download/v2.2.1/apktool_2.2.1.jar
 
 # Download and unzip dex2jar
 wget https://bitbucket.org/pxb1988/dex2jar/downloads/dex2jar-2.0.zip
